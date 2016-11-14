835d22fcd1fd8d8415bc34959c8f6d71fd394c69
Toshi Piazza <toshi.piazza@gmail.com>
2016-11-14 02:53:26 -0500

    Adds mid-project review

--- cfg.cpp
+++ cfg.cpp
@@ -1,25 +1,32 @@
 #define DEBUG_TYPE "cfg"
 #include "llvm/IR/Function.h"
 #include "llvm/IR/InstVisitor.h"
-#include "llvm/IR/Instruction.h"
-#include "llvm/Pass.h"
-#include "llvm/Support/Debug.h"
 
 using namespace llvm;
 
 namespace {
 
-  struct CFG : public FunctionPass {
-    static char ID;
-    CFG() : FunctionPass(ID) {}
-
-    bool runOnFunction(Function &F) override {
-      // TODO
-      return false;
+  struct CBRVisitor :
+      public InstVisitor<CBRVisitor> {
+    bool changed = false;
+    void visitBranchInst(BranchInst &BI) {
+      if (BI.isConditional()) {
+        // victimize the true and false branches
+      }
     }
   };
+
+  struct CFG : public BasicBlockPass {
+    static char ID;
+    CFG() : BasicBlockPass(ID) { }
+    bool runOnBasicBlock(BasicBlock &B) override {
+      CBRVisitor cbrVisitor;
+      cbrVisitor.visit(B);
+      return cbrVisitor.changed;
+    }
+  };
+
 }
 
 char CFG::ID = 0;
 static RegisterPass<CFG> X("cfg", "Control Flow Graph Pass");
-// vim:tabstop=2:set shiftwidth=2:
---
+++ mid-project.md
@@ -0,0 +1,18 @@
+Mid Project Review
+==================
+
+## Current Progress
+
+A website has been made outlining the high-level overview of our project. A checklist for
+the deliverables for our project include:
+
+[ ] construct a basic block pass which instruments each conditional branch  
+[ ] write `__not_taken()` and `__taken()` functions for the above instrumentation  
+[ ] build a python script which colors an IDA control flow graph  
+[ ] build a python script which augments IDA control flow for indirect branches  
+
+So far, the basic block pass has been attempted. Currently the pass runs, and is able to
+accurately instrument each conditional branch and modify its target and fallthrough
+branches. The stdlibc++ functionality has not been written that will be our trampolines
+for keeping track of the branches, but is underway. Once these have been completed, the
+python scripts will be written augmenting IDA's current funcionality.
